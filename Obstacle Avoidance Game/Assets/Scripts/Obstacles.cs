f1d5ef43e3b84a1f40e607ec41a2b47add36826e
beyza-parlak256 <beyza.parlak98@erzurum.edu.tr>
2022-10-16 20:21:26 +0300

    I created the score text. Score increased when character didn't touch obstacles

--- Obstacle Avoidance Game/Assets/Scripts/Obstacles.cs
+++ Obstacle Avoidance Game/Assets/Scripts/Obstacles.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Obstacles : MonoBehaviour
 {
-  
+
+    private static int score;
+    public TextMeshProUGUI scoreText;
+    void Update()
+    {
+        // int -> stringe geçiþte ToString() kullanýlýr
+        scoreText.text = score.ToString();
+    }
 
     void OnCollisionEnter2D(Collision2D contact)
     {
@@ -13,6 +21,7 @@ public class Obstacles : MonoBehaviour
 
         if(contact.gameObject.tag == "ControlStick")
         {
+            score += Random.Range(5,16);
             transform.position = new Vector2(xPosition,yPosition);
         }
         if(contact.gameObject.tag == "Character")
@@ -20,5 +29,6 @@ public class Obstacles : MonoBehaviour
             transform.position = new Vector2(xPosition, yPosition);
         }
     }
+    
 
 }
